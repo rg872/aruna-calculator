c4f4d6fe34b0a914528e491d829f27230b3f4599
Rafie Gilang <rafiegilang@Rafies-MacBook-Pro.local>
2020-09-23 09:58:53 +0700

    feat: create input-number component

---
+++ src/components/input-number/input-number.component.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+import './input-number.component.scss'
+
+export default function InputNumber(props) {
+  const { number, setNumber } = props;
+
+  return (
+    <input className="input-number" type="number" value={number} onChange={(e) => setNumber(e.target.value)}/>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+import InputNumber from './components/input-number/input-number.component.jsx'
 
 import './index.scss'
 
 function Calculator() {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [thirdNumber, setThirdNumber] = useState(0);
+
   return (
     <div className="container">
-      PROJECT INIT
+      <InputNumber number={firstNumber} setNumber={setFirstNumber}/>
+      <InputNumber number={secondNumber} setNumber={setSecondNumber}/>
+      <InputNumber number={thirdNumber} setNumber={setThirdNumber}/>
     </div>
   )
 }
